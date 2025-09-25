d4417f28c209c45bbe5c53bac58c28fcc036222c
Deyvid333 <rm94999@estudante.fieb.edu.br>
2025-09-25 11:49:05 -0300

    cosumo de rotas

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,9 +1,17 @@
+//ferramenta de consumo de rotas ou endpoints do backend
+
 import axios from "axios"
+//hooks do react para controlar estados e rederização
 import { useState, useEffect } from "react"
 const Produtos = () => {
+    //estado com a lista de pizzas
     const [pizzas, setPizzas] = useState([])
     // Objeto com a lista de pizzas
     useEffect(()=>{
+
+    //get - busca os dados da rota ou endpoint
+    //then metodo- assincrono, espera o retorno da requisição
+    //catch - tratamento de erros da requisição
         axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
         .then(response=>{
             setPizzas(response.data.data)
